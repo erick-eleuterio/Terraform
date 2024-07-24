3171538fc1f3832877087bcce3dae76d72a4dbd1
Erick Eleuterio <erickeleuterio2015@gmail.com>
2024-07-23 22:55:35 -0300

    Tipos de blocos do Terraform - 23/07/2024

---
+++ blocos.tf
@@ -0,0 +1,32 @@
+terraform {
+  
+}
+
+provider "aws" {
+  
+}
+
+resource "aws_instance" "My_VM_1" {
+  
+}
+
+data "aws-ami" "image" {
+  
+}
+
+module "network" {
+  
+}
+
+variable "vm_name" {
+  
+}
+
+
+output "vm1_ip" {
+  
+}
+
+locals {
+  
+}
