937fd90db3a91f592bef47c64e87eb999f2e38dd
Erick Eleutério <140910314+erick-eleuterio@users.noreply.github.com>
2024-07-29 22:09:51 -0300

    Update bucket.tf

--- AWS_BUCKET/bucket.tf
+++ AWS_BUCKET/bucket.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "bucket" {
     Environment = "Curso Udemy"
   }
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-bucket-erick-eleuterio-curso-udemy"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Curso Udemy"
+  }
+}
